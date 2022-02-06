cc517283f0a01c97fa8ea0bbe6e2cc8e59ab4d67
Myoung <self-made@Myoungs-MacBook-Pro.local>
2022-02-05 22:19:37 -0700

    merge latest master.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -80,6 +80,8 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use 'mfussenegger/nvim-dap' -- debugger 
+  use "Pocco81/DAPInstall.nvim" -- debugger installer
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
